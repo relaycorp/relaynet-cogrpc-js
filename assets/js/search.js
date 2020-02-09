fd71c26c97e3d4c58d10b9798927e7e89df0adc3
gnarea <gnarea@users.noreply.github.com>
2020-02-09 11:43:53 +0000

    fix: Expose gRPC bidi stream call [skip ci] 0d524e265d51e5dc22cbe78fc4004d0d78c9a716

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,3 @@
 var typedoc = typedoc || {};
             typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#cargo_delivery_grpc_service.__type","classes":"tsd-kind-type-literal tsd-parent-kind-variable tsd-is-not-exported","parent":"CARGO_DELIVERY_GRPC_SERVICE"}]};
+            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"globals.html#cargodeliverystream.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"CargoDeliveryStream"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#cargo_delivery_grpc_service.__type","classes":"tsd-kind-type-literal tsd-parent-kind-variable tsd-is-not-exported","parent":"CARGO_DELIVERY_GRPC_SERVICE"}]};
