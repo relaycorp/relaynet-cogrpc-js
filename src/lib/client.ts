82ff26277edf05b0a385c98d4f7b1ef3bab66e4a
Gus Narea <gnarea@users.noreply.github.com>
2020-05-22 23:17:39 +0100

    fix: Download TLS certificate upfront when connecting to a courier (#41)
    * wip
    
    * fix: Download TLS certificate upfront when connecting to a courier
    
    As a workaround for https://github.com/grpc/grpc-node/issues/663
    
    Fixes #22.
    
    * Fix formatting issue
    
    * refactor

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,8 +1,10 @@
 import { CargoDeliveryRequest, RelaynetError } from '@relaycorp/relaynet-core';
+import checkIp from 'check-ip';
 import { get as getEnvVar } from 'env-var';
 import * as grpc from 'grpc';
 import pipe from 'it-pipe';
 import * as toIterable from 'stream-to-it';
+import * as tls from 'tls';
 import uuid from 'uuid-random';
 
 import {
@@ -14,14 +16,21 @@ import {
 
 const DEADLINE_SECONDS = 3;
 
+const COURIER_GRPC_PORT = 21473;
+
 export class CogRPCError extends RelaynetError {}
 
 // tslint:disable-next-line:max-classes-per-file
 export class CogRPCClient {
+  public static async init(serverUrl: string): Promise<CogRPCClient> {
+    const credentials = await createCredentials(serverUrl);
+    return new CogRPCClient(serverUrl, credentials);
+  }
+
   protected readonly grpcClient: InstanceType<typeof CargoRelayClient>;
 
-  constructor(serverUrl: string) {
-    this.grpcClient = new CargoRelayClient(serverUrl, this.createCredentials(serverUrl));
+  protected constructor(serverUrl: string, credentials: grpc.ChannelCredentials) {
+    this.grpcClient = new CargoRelayClient(serverUrl, credentials);
   }
 
   public close(): void {
@@ -107,16 +116,53 @@ export class CogRPCClient {
       throw new CogRPCError(error, 'Unexpected error while collecting cargo');
     }
   }
+}
 
-  protected createCredentials(serverUrl: string): grpc.ChannelCredentials {
-    const serverUrlParts = new URL(serverUrl);
-    const useTls = serverUrlParts.protocol === 'https:';
-    const isTlsRequired = getEnvVar('COGRPC_REQUIRE_TLS').default('true').asBool();
-    if (!useTls && isTlsRequired) {
-      throw new CogRPCError(`Cannot connect to ${serverUrl} because TLS is required`);
-    }
-    return useTls ? grpc.credentials.createSsl() : grpc.credentials.createInsecure();
+async function createCredentials(serverUrl: string): Promise<grpc.ChannelCredentials> {
+  const serverUrlParts = new URL(serverUrl);
+  const useTls = serverUrlParts.protocol === 'https:';
+  const isTlsRequired = getEnvVar('COGRPC_REQUIRE_TLS').default('true').asBool();
+  if (!useTls && isTlsRequired) {
+    throw new CogRPCError(`Cannot connect to ${serverUrl} because TLS is required`);
   }
+  return useTls
+    ? createTlsCredentials(serverUrlParts.hostname, serverUrlParts.port)
+    : grpc.credentials.createInsecure();
+}
+
+async function createTlsCredentials(host: string, port: string): Promise<grpc.ChannelCredentials> {
+  const ipInfo = checkIp(host);
+  if (!ipInfo.isValid || ipInfo.isPublicIp) {
+    // The host name is a domain name or a public IP address, so we should let the usual certificate
+    // validation take place.
+    return grpc.credentials.createSsl();
+  }
+
+  // The host is a private IP address so it's going to have a self-issued certificate. Due to a
+  // bug in grpc-node (https://github.com/grpc/grpc-node/issues/663), we can't simply instruct
+  // the client to accept self-issued certificates, so we have to download the TLS certificate
+  // from the server so we can pass it explicitly to the client.
+  const portNumber = port === '' ? COURIER_GRPC_PORT : parseInt(port, 10);
+  const certificatePem = await retrieveCertificatePem(host, portNumber);
+  return grpc.credentials.createSsl(certificatePem);
+}
+
+async function retrieveCertificatePem(host: string, port: number): Promise<Buffer> {
+  return new Promise((resolve) => {
+    const tlsSocket = tls.connect({ host, port, rejectUnauthorized: false }, () => {
+      const certificateDer: Buffer = tlsSocket.getPeerCertificate().raw;
+      tlsSocket.end();
+      return resolve(derCertificateToPem(certificateDer));
+    });
+  });
+}
+
+function derCertificateToPem(derBuffer: Buffer): Buffer {
+  const lines = derBuffer.toString('base64').match(/.{1,64}/g) as RegExpMatchArray;
+  const pemString = [`-----BEGIN CERTIFICATE-----`, ...lines, `-----END CERTIFICATE-----`].join(
+    '\n',
+  );
+  return Buffer.from(pemString);
 }
 
 function makeDeadline(): Date {
